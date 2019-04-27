863ccf751c384cfd6132a1cfcd544aa18110f70f
z3nth10n <z3nth10n@gmail.com>
2019-04-27 21:33:07 +0200

    Initial commit

---
+++ HtmlFormatterTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TidyManaged;
+
+namespace HtmlFormatterTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var client = new RestClient("https://steamcommunity.com/workshop/browse/?appid=4000");
+            string dirtyHtml = client.Get(new RestRequest()).Content;
+
+            string cleanHtml = CleanHtml(dirtyHtml);
+
+            string filePath = Path.Combine(Assembly.GetExecutingAssembly().Location, "test.html");
+            File.WriteAllText(filePath, cleanHtml);
+
+            Console.Read();
+        }
+
+        private static string CleanHtml(string dirtyHtml)
+        {
+            using (Document doc = Document.FromString(dirtyHtml))
+            {
+                doc.OutputBodyOnly = AutoBool.Yes;
+                doc.Quiet = true;
+                doc.CleanAndRepair();
+
+                return doc.Save();
+            }
+        }
+    }
+}
